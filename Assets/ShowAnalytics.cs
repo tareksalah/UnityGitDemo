c0d2495ef6ef11a8b5d4430b34356e49eb15d2ef
Tarek Salah <tarek.salah.kamel@gmail.com>
2017-03-10 10:44:27 +0200

    CS files of the Analytics project

---
+++ Assets/ShowAnalytics.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+
+public class ShowAnalytics : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+
+
+		Gender gender = Gender.Female;
+		Analytics.SetUserGender (gender);
+
+		int birthYear = 2000;
+		Analytics.SetUserBirthYear (birthYear);
+
+		LevelUp ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void LevelUp ()
+	{
+		Analytics.Transaction ("LevelUp", 1, "Euro", null, null); 
+
+
+		Analytics.CustomEvent ("LevelUp", new Dictionary<string, object> {
+			{ "newlevel", 2 },
+			{ "trigger", "purchase" },
+		});
+
+	}
+}
